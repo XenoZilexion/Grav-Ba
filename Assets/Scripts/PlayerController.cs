22119928748fd88880f322a23efaf6814a8b6fac
Rishi Barnwal <barnwal.rishi@gmail.com>
2020-03-03 12:54:05 -0500

    Added movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,28 +5,58 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1.0f;
-    public float jumpHeight = 5.0f;
 
+    public Vector2 jumpVector;
 
-    private Vector2 inputVector;
-    private Rigidbody2D rb2D;
+    public bool grounded;
+    public LayerMask whatIsGround;
+    public bool stoppedJumping;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+
+    private float horizontalInput;
+    private Rigidbody2D rb;
+    private Vector3 positionChange;
 
     void Start()
     {
-        rb2D = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        CheckGround();
+    }
+
+    private void FixedUpdate()
+    {
+        ControllerInput();
     }
 
     private void ControllerInput()
     {
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        positionChange = transform.position;
 
+        positionChange.x = positionChange.x + (horizontalInput * moveSpeed);
 
+        rb.MovePosition(positionChange);
+
+        if (Input.GetKeyDown(KeyCode.W) && grounded)
+        {
+            Jump();
+        }
+    }
+
+    private void CheckGround()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = jumpVector;
     }
 
 }
