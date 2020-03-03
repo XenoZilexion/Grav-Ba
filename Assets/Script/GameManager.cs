d05bafc1ace0a6adac0ba1afcea6fb08033e02e0
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2020-03-03 14:32:19 -0500

    Added GameManager and Timer Script

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Text timerText;
+    [SerializeField] private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+    }
+
+    void Timer()
+    {
+        timer -= Time.deltaTime;
+        timerText.text = timer.ToString("Time: " + Mathf.FloorToInt(timer));
+    }
+}
