24529ee23e01d8bb4fa0aa875b616a137f0655d3
Rishi Barnwal <barnwal.rishi@gmail.com>
2020-03-03 10:01:24 -0500

    Added spritesheet and control scripts for prototyping

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    public float jumpHeight = 5.0f;
+
+
+    private Vector2 inputVector;
+    private Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void ControllerInput()
+    {
+        inputVector.x = Input.GetAxis("Horizontal");
+        inputVector.y = Input.GetAxis("Vertical");
+
+
+    }
+
+}
