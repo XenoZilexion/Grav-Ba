a4edfd458b5c78aea9bbcf9e9e0e365a062bf87c
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2020-03-04 12:40:15 -0500

    Added a test score function

---
+++ Assets/Script/ClickScoreTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickScoreTest : MonoBehaviour
+{
+    public GameManager scoreManager;
+
+    private void OnMouseDown()
+    {
+        scoreManager.Score();
+
+    }
+}
--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,16 +7,58 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private Text timerText;
     [SerializeField] private float timer;
+    public Text scoreText;
+    [SerializeField] private float score = 0;
+    private string scoreFormat = "#";
+    public Text multiplierText;
+    public float multiplier;
+    public float comboCount;
+    public float addScore;
+    public float comboWindow;
 
     // Update is called once per frame
     void Update()
     {
-        Timer();
+        UpdateHud();
     }
 
-    void Timer()
+    void UpdateHud()
     {
         timer -= Time.deltaTime;
         timerText.text = timer.ToString("Time: " + Mathf.FloorToInt(timer));
+        scoreText.text = "Score: " + score.ToString(scoreFormat);
+        multiplierText.text = multiplier.ToString("Multiplier: " + multiplier);
+    }
+    public void Score()
+    {
+        if (comboCount == 0)
+        {
+            multiplier *= 1;
+            score += addScore;
+            comboCount++;
+            StartCoroutine("Combo");
+        }
+
+        else if (comboCount >= 5)
+        {
+            multiplier *= 2;
+            score += (addScore * 2);
+            comboCount++;
+            StopCoroutine("Combo");
+        }
+       
+        else if (comboCount >= 10)
+        {
+            multiplier *= 2;
+            score += (addScore * 4);
+            comboCount++;
+            StopCoroutine("Combo");
+        }
+    }
+
+    IEnumerator Combo()
+    {
+        yield return new WaitForSeconds(comboWindow);
+        comboCount = 0;
     }
 }
