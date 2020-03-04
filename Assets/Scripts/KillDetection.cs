d9b28ae36e37362d7b4c3bd2a7be30320474a28d
Ryan Matthews <ryanmatthews334@gmail.com>
2020-03-04 13:27:37 -0500

    Added AI Enemy, Tesla traps, UI Controller for time and score, collision detection for collectables and attacking enemies

---
+++ Assets/Scripts/KillDetection.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillDetection : MonoBehaviour
+{
+    public Vector3 restartPosition = new Vector3(-6.6f, 0.8f, 0);
+
+    public void respawnPlayer()
+    {
+        Vector3 pScale = Vector3.one;
+        transform.position = restartPosition;
+        transform.localScale = new Vector3(1 / pScale.x, 1 / pScale.y, 1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //This is checking the collision for a gameObject that has the
+        //Script 'KillPlayer' Attached to it
+        GameObject checkCollision = collision.gameObject;
+        KillPlayer canKill = checkCollision.GetComponent<KillPlayer>();
+
+        if (canKill != null)
+        {
+            respawnPlayer();
+        }
+    }
+    
+}
