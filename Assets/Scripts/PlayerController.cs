fb3c4d3801abe734803898a4eecbb083a2333eff
Zilexion <59101106+XenoZilexion@users.noreply.github.com>
2020-03-05 16:49:59 -0500

    this should properly fix the jump eating bug.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -55,6 +55,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        ControllerInput();
         //Debug.Log(currentGravity);
         CheckGround();
         Fall();
@@ -64,7 +65,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        ControllerInput();
+        
     }
 
     private void ControllerInput()
