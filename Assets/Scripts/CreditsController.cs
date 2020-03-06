acafda75f1bb7ec6a2a8b26e57f7b8ad2e92dcc1
Joseph Ryder <joseph.ryder@sheridancollege.ca>
2020-03-05 22:18:15 -0500

    Added background art to level

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreditsController : MonoBehaviour
+{
+    public Text creditText;
+    public float credits;
+    // Start is called before the first frame update
+    void Start()
+    {
+        creditText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("3"))
+        {
+            credits += 0.25f;               
+        }
+        if (credits <= 0.99)
+        {
+            creditText.text = ("Credits: " + credits);          
+        }
+        else
+        {
+            creditText.text = ("Press any button to play " + credits);
+            if (Input.GetKey("1"))
+            {
+                SceneManager.LoadScene(1);
+            }
+        }       
+    }
+}
