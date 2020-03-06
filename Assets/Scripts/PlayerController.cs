619527dbf44175a8e21d85bf53ed1f05b25df4c3
Rishi Barnwal <barnwal.rishi@gmail.com>
2020-03-06 01:24:57 -0500

    Changed control mappings damnit unity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -142,7 +142,7 @@ public class PlayerController : MonoBehaviour
             }
             */
 
-            if (Input.GetKeyDown(KeyCode.C))
+            if (Input.GetKeyDown(KeyCode.Z))
             {
                 Debug.Log("Key presed");
                 attemptJump = true;
